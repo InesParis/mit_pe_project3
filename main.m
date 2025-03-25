54730ac1f7a890293b77b4f641d080156566c642
InesParis <inesparisarranz@gmail.com>
2025-03-25 12:54:16 +0100

    updated JS
    trying to apply the matlab logic to the js

--- main.m
+++ main.m
@@ -12,6 +12,19 @@ fileID = fopen('c:\Users\ines.paris\OneDrive - Global Alumni\VS CODE\mit_pe_proj
 fprintf(fileID, DSM_json);
 fclose(fileID);
 
+% Compute theoretical cost evolution
+t_plot = unique(round(10.^[0 : .05 : 7]'));
+t0     = factorial(d+1) / (d^(d+2)) * n;
+c_ave  = (t_plot/t0 + 1).^(-1/d);
+
+% Export cost evolution to JSON
+cost_data.time = t_plot;
+cost_data.cost = c_ave;
+cost_json = jsonencode(cost_data);
+fileID = fopen('c:\Users\ines.paris\OneDrive - Global Alumni\VS CODE\mit_pe_project3\cost_data.json', 'w');
+fprintf(fileID, cost_json);
+fclose(fileID);
+
 % Simulate DSM
 % kmax   = 1000; % # of success to simulate
 % tmax   = 1e8;  % max # of time steps to simulate
